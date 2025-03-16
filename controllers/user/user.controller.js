5e2b692c1c3edb1481b6c2f20fc2e7d095fe8a54
Abhay5855 <ap3919529@gmail.com>
2025-03-16 16:23:10 +0530

    feat:user registration with validation, post route

--- controllers/user/user.controller.js
+++ controllers/user/user.controller.js
@@ -1,5 +1,48 @@
+import bcrypt from "bcrypt";
+import User from "../../models/user/UserModel.js";
+import { userSchema } from "../../validations/user.js";
+
 const registerUser = async (req, res) => {
-  await res.send("User is registered successfully");
+  const { error, value } = userSchema.validate(req.body, { abortEarly: false });
+  const saltRounds = 10;
+
+  try {
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(value.password, saltRounds);
+
+    //check if email already exists
+    const checkIfEmailExists = await User.findOne({ email: value.email });
+    if (checkIfEmailExists) {
+      return res.status(409).json({
+        message: "Email already exists !",
+      });
+    }
+
+    //Validations message
+    if (error) {
+      return res.status(500).json({
+        message: error.details.map((err) => err.message),
+      });
+    }
+
+    const user = new User({
+      ...value,
+      password: hashedPassword,
+    });
+
+    //Save the user
+    await user.save();
+
+    res.status(201).json({
+      status: 201,
+      message: "User creted successfully",
+      user,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
 };
 
 export { registerUser };
--- models/user/UserModel.js
+++ models/user/UserModel.js
@@ -1,6 +1,42 @@
 import { Schema } from "mongoose";
+import mongoose from "mongoose";
 
-const userSchema = new Schema({}, { timestamps: true });
+const userSchema = new Schema(
+  {
+    first_name: {
+      type: String,
+    },
+    last_name: {
+      type: String,
+    },
+    email: {
+      type: String,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    virtuals: {
+      fullName: {
+        get() {
+          return this.first_name + " " + this.last_name;
+        },
+      },
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
