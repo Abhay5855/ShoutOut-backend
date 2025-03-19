024bea0af7e26cc42492ddf4453cf8c67643090f
Abhay5855 <ap3919529@gmail.com>
2025-03-19 09:57:55 +0530

    feat:Implement login functionality with generating access_token with login and storing it in a cookie with middleware created to check the valid token

--- routes/user/user.routes.js
+++ routes/user/user.routes.js
@@ -2,12 +2,18 @@ import express from "express";
 import {
   registerUser,
   verifyEmail,
+  authenticateUser,
 } from "../../controllers/user/user.controller.js";
+import { verifyIsLoggedIn } from "../../middleware/index.js";
 
 const router = express.Router();
 
 router.post("/signup", registerUser);
 
 router.get("/verify/:token", verifyEmail);
+
+router.post("/login", authenticateUser);
+
+router.get("/test", verifyIsLoggedIn);
 
 export default router;
